b9202344bd60f451a4b41a5a8773912ef4c03c4f
Okan Gurbuz <okangurbuz06@gmail.com>
2022-08-08 17:42:03 +0200

    08/08

--- src/test/java/stepdefinitions/uiSteps/US012_UiSteps.java
+++ src/test/java/stepdefinitions/uiSteps/US012_UiSteps.java
@@ -1,11 +1,14 @@
 package stepdefinitions.uiSteps;
 
-import io.cucumber.java.en.*;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import pages.US012_Pages;
 import utilities.ConfigurationReader;
 import utilities.Driver;
 
 public class US012_UiSteps {
 
+    US012_Pages pages = new US012_Pages();
 
 
     @Given("Kullanici  Medunna sayfasina gider")
@@ -14,38 +17,34 @@ public class US012_UiSteps {
     }
     @Then("Kullanici  giris simgesini tiklar")
     public void kullanici_giris_simgesini_tiklar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        pages.signeIn.click();
     }
     @Then("Kullanici  acilan menuden sign in i tiklar")
     public void kullanici_acilan_menuden_sign_in_i_tiklar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        pages.login.click();
+
     }
     @Then("Kullanici  Kullanici ismini girer")
     public void kullanici_kullanici_ismini_girer() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        pages.userName.sendKeys("doktor67");
     }
     @Then("Kullanici  Kullanici sifresini girer")
     public void kullanici_kullanici_sifresini_girer() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        pages.password.sendKeys("Doktor67");
     }
     @Then("Kullanici  sign in i butonunu tiklayarak kullanici sayfasina baglanir")
     public void kullanici_sign_in_i_butonunu_tiklayarak_kullanici_sayfasina_baglanir() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        pages.signIn_boutton.click();
     }
     @Then("Cikan sayfadan My Pages basligini tiklar")
     public void cikan_sayfadan_my_pages_basligini_tiklar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        pages.myPages.click();
     }
     @Then("Kullanici My Appointments sekmesini tiklayarak rendevu sayfasina gecis yapar")
     public void kullanici_my_appointments_sekmesini_tiklayarak_rendevu_sayfasina_gecis_yapar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        pages.myAppointments.click();
+
     }
     @Then("Kullanici  Edit buttonuna tiklar")
     public void kullanici_edit_buttonuna_tiklar() {
