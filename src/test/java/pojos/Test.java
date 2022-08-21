5f528794bc75f66c901ef9bc442c70583759e579
Ersin Akdemir <akdemirersin.de@gmail.com>
2022-08-21 12:15:12 +0200

    c

--- src/test/java/pojos/Test.java
+++ src/test/java/pojos/Test.java
@@ -34,5 +34,70 @@ public class Test {
     private String defaultValMin;
     private String defaultValMax;
 
+    public Test() {
+    }
 
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDefaultValMin() {
+        return defaultValMin;
+    }
+
+    public void setDefaultValMin(String defaultValMin) {
+        this.defaultValMin = defaultValMin;
+    }
+
+    public String getDefaultValMax() {
+        return defaultValMax;
+    }
+
+    public void setDefaultValMax(String defaultValMax) {
+        this.defaultValMax = defaultValMax;
+    }
 }
